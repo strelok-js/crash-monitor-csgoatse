a9d0d2532e65e10255337d214497724459763fe6
Ar4ys <artur.yurko1@gmail.com>
2019-10-28 02:28:38 +0000

    Add case, when observer should trigger

--- crash-monitor.js
+++ crash-monitor.js
@@ -7,8 +7,16 @@ const config = {
 const observer = new MutationObserver(callback);
 observer.observe(target, config);
 
+window.lose = false;
+
 function callback(mutationList, observer) {
 	mutationList.forEach(mutation => {
-		console.log(mutation);
+		if (mutation.target.classList.contains("lose") && !window.lose) {
+			console.log(mutation);
+			window.lose = true;
+		}
+
+		if (!mutation.target.classList.contains("lose"))
+			window.lose = false
 	});
 }
